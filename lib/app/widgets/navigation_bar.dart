9c1e72cbd335456ad62bcb7690c2c8fbea147599
Priyanka <arorapriyanka1801@gmail.com>
2024-05-04 18:14:17 +0530

    navigation bar update

--- lib/app/widgets/navigation_bar.dart
+++ lib/app/widgets/navigation_bar.dart
@@ -1,36 +1,65 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
 import 'package:marvel_app/app/constants/asset_constants.dart';
 import 'package:marvel_app/app/constants/text_constants.dart';
 import 'package:marvel_app/app/theme/app_colors.dart';
+
 import 'package:marvel_app/app/theme/app_textstyles.dart';
 
 class AppBottomNavigationBar {
   static Widget navigationBar() {
-    return NavigationBar(
-      backgroundColor: AppColors.primaryBlack,
-      destinations: [
-        Column(
-          children: [
-            Image.asset(AppIcons.list),
-            const Text(
-              AppText.characters,
-              style: AppTextStyles.white_14_500w,
+    return CurvedNavigationBar(
+        height: 65.h,
+        buttonBackgroundColor: AppColors.primaryBlack,
+        index: 1,
+        color: AppColors.primaryBlack,
+        backgroundColor: AppColors.primaryWhite,
+        items: [
+          Padding(
+            padding: const EdgeInsets.all(14.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Image.asset(AppIcons.list),
+                const Text(
+                  AppText.characters,
+                  style: AppTextStyles.white_14_500w,
+                ),
+              ],
             ),
-          ],
-        ),
-        Column(
-          children: [
-            Image.asset(AppIcons.home),
-            const Text(AppText.home, style: AppTextStyles.white_14_500w,),
-          ],
-        ),
-        Column(
-          children: [
-            Image.asset(AppIcons.more),
-            const Text(AppText.more, style: AppTextStyles.white_14_500w,),
-          ],
-        )
-      ],
-    );
+          ),
+          Padding(
+            padding: const EdgeInsets.all(14.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Image.asset(AppIcons.home),
+                const Text(
+                  AppText.home,
+                  style: AppTextStyles.white_14_500w,
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(14.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Image.asset(AppIcons.more),
+                const Text(
+                  AppText.more,
+                  style: AppTextStyles.white_14_500w,
+                ),
+              ],
+            ),
+          )
+        ]);
   }
 }
