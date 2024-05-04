39a036c68ed12dd04070961cf521c9f69872ca69
Priyanka <arorapriyanka1801@gmail.com>
2024-05-04 12:22:31 +0530

    update

--- lib/app/pages/SplashScreen/splash_binding.dart
+++ lib/app/pages/SplashScreen/splash_binding.dart
@@ -1,7 +1,7 @@
 import 'package:get/get.dart';
 
 import 'splash_controller.dart';
-import 'splash_presenter.dart';
+// import 'splash_presenter.dart';
 
 class SplashBinding extends Bindings {
   @override
